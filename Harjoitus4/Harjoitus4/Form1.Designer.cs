5afb17b51189e87214f179b8d07ad56a4c8b0f99
unknown <tomas.roulamo@edu.keuda.fi>
2021-08-26 10:15:29 +0300

    Harjotus 4

---
+++ Harjoitus4/Harjoitus4/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace Harjoitus4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SynttariDT = new System.Windows.Forms.DateTimePicker();
+            this.LaskeBT = new System.Windows.Forms.Button();
+            this.VuosinaLB = new System.Windows.Forms.Label();
+            this.KuukausinaLB = new System.Windows.Forms.Label();
+            this.PaivinaLB = new System.Windows.Forms.Label();
+            this.TunteinaLB = new System.Windows.Forms.Label();
+            this.MinuutteinaLB = new System.Windows.Forms.Label();
+            this.SekunteinaLB = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // SynttariDT
+            // 
+            this.SynttariDT.Location = new System.Drawing.Point(91, 91);
+            this.SynttariDT.Name = "SynttariDT";
+            this.SynttariDT.Size = new System.Drawing.Size(200, 20);
+            this.SynttariDT.TabIndex = 0;
+            // 
+            // LaskeBT
+            // 
+            this.LaskeBT.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LaskeBT.Location = new System.Drawing.Point(361, 85);
+            this.LaskeBT.Name = "LaskeBT";
+            this.LaskeBT.Size = new System.Drawing.Size(86, 31);
+            this.LaskeBT.TabIndex = 1;
+            this.LaskeBT.Text = "Laske";
+            this.LaskeBT.UseVisualStyleBackColor = true;
+            this.LaskeBT.Click += new System.EventHandler(this.LaskeBT_Click);
+            // 
+            // VuosinaLB
+            // 
+            this.VuosinaLB.AutoSize = true;
+            this.VuosinaLB.Location = new System.Drawing.Point(91, 169);
+            this.VuosinaLB.Name = "VuosinaLB";
+            this.VuosinaLB.Size = new System.Drawing.Size(37, 13);
+            this.VuosinaLB.TabIndex = 2;
+            this.VuosinaLB.Text = "vuotta";
+            this.VuosinaLB.Visible = false;
+            // 
+            // KuukausinaLB
+            // 
+            this.KuukausinaLB.AutoSize = true;
+            this.KuukausinaLB.Location = new System.Drawing.Point(91, 206);
+            this.KuukausinaLB.Name = "KuukausinaLB";
+            this.KuukausinaLB.Size = new System.Drawing.Size(55, 13);
+            this.KuukausinaLB.TabIndex = 3;
+            this.KuukausinaLB.Text = "kuukautta";
+            this.KuukausinaLB.Visible = false;
+            // 
+            // PaivinaLB
+            // 
+            this.PaivinaLB.AutoSize = true;
+            this.PaivinaLB.Location = new System.Drawing.Point(91, 241);
+            this.PaivinaLB.Name = "PaivinaLB";
+            this.PaivinaLB.Size = new System.Drawing.Size(39, 13);
+            this.PaivinaLB.TabIndex = 4;
+            this.PaivinaLB.Text = "päivää";
+            this.PaivinaLB.Visible = false;
+            // 
+            // TunteinaLB
+            // 
+            this.TunteinaLB.AutoSize = true;
+            this.TunteinaLB.Location = new System.Drawing.Point(206, 169);
+            this.TunteinaLB.Name = "TunteinaLB";
+            this.TunteinaLB.Size = new System.Drawing.Size(33, 13);
+            this.TunteinaLB.TabIndex = 5;
+            this.TunteinaLB.Text = "tuntia";
+            this.TunteinaLB.Visible = false;
+            // 
+            // MinuutteinaLB
+            // 
+            this.MinuutteinaLB.AutoSize = true;
+            this.MinuutteinaLB.Location = new System.Drawing.Point(206, 206);
+            this.MinuutteinaLB.Name = "MinuutteinaLB";
+            this.MinuutteinaLB.Size = new System.Drawing.Size(49, 13);
+            this.MinuutteinaLB.TabIndex = 6;
+            this.MinuutteinaLB.Text = "minuuttia";
+            this.MinuutteinaLB.Visible = false;
+            // 
+            // SekunteinaLB
+            // 
+            this.SekunteinaLB.AutoSize = true;
+            this.SekunteinaLB.Location = new System.Drawing.Point(206, 241);
+            this.SekunteinaLB.Name = "SekunteinaLB";
+            this.SekunteinaLB.Size = new System.Drawing.Size(47, 13);
+            this.SekunteinaLB.TabIndex = 7;
+            this.SekunteinaLB.Text = "sekuntia";
+            this.SekunteinaLB.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.SekunteinaLB);
+            this.Controls.Add(this.MinuutteinaLB);
+            this.Controls.Add(this.TunteinaLB);
+            this.Controls.Add(this.PaivinaLB);
+            this.Controls.Add(this.KuukausinaLB);
+            this.Controls.Add(this.VuosinaLB);
+            this.Controls.Add(this.LaskeBT);
+            this.Controls.Add(this.SynttariDT);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker SynttariDT;
+        private System.Windows.Forms.Button LaskeBT;
+        private System.Windows.Forms.Label VuosinaLB;
+        private System.Windows.Forms.Label KuukausinaLB;
+        private System.Windows.Forms.Label PaivinaLB;
+        private System.Windows.Forms.Label TunteinaLB;
+        private System.Windows.Forms.Label MinuutteinaLB;
+        private System.Windows.Forms.Label SekunteinaLB;
+    }
+}
+
