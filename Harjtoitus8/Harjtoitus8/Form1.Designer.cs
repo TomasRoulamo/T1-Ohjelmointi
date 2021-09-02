ea1d3be806aba3dacd41d7bdc6dbe97106f55431
unknown <tomas.roulamo@edu.keuda.fi>
2021-09-02 10:54:16 +0300

    Teht 8 tehty ja toimii

---
+++ Harjtoitus8/Harjtoitus8/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Harjtoitus8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.NumeroTB = new System.Windows.Forms.TextBox();
+            this.ButtonBT = new System.Windows.Forms.Button();
+            this.VastausLB = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(142, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(481, 55);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Roomalaiset numerot";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 153);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(582, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Anna numero 1-3999 välillä, niin muutan sen roomalaiseksi:";
+            // 
+            // NumeroTB
+            // 
+            this.NumeroTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NumeroTB.Location = new System.Drawing.Point(616, 147);
+            this.NumeroTB.Name = "NumeroTB";
+            this.NumeroTB.Size = new System.Drawing.Size(100, 31);
+            this.NumeroTB.TabIndex = 2;
+            // 
+            // ButtonBT
+            // 
+            this.ButtonBT.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ButtonBT.Location = new System.Drawing.Point(627, 233);
+            this.ButtonBT.Name = "ButtonBT";
+            this.ButtonBT.Size = new System.Drawing.Size(89, 31);
+            this.ButtonBT.TabIndex = 3;
+            this.ButtonBT.Text = "Muuta";
+            this.ButtonBT.UseVisualStyleBackColor = true;
+            this.ButtonBT.Click += new System.EventHandler(this.ButtonBT_Click);
+            // 
+            // VastausLB
+            // 
+            this.VastausLB.AutoSize = true;
+            this.VastausLB.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.VastausLB.Location = new System.Drawing.Point(62, 233);
+            this.VastausLB.Name = "VastausLB";
+            this.VastausLB.Size = new System.Drawing.Size(113, 31);
+            this.VastausLB.TabIndex = 4;
+            this.VastausLB.Text = "Vastaus";
+            this.VastausLB.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.VastausLB);
+            this.Controls.Add(this.ButtonBT);
+            this.Controls.Add(this.NumeroTB);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox NumeroTB;
+        private System.Windows.Forms.Button ButtonBT;
+        private System.Windows.Forms.Label VastausLB;
+    }
+}
+
