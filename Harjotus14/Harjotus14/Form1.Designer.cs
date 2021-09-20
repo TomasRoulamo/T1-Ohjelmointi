67386e66ceddfd653e58195090a83db48bee904b
unknown <tomas.roulamo@edu.keuda.fi>
2021-09-20 13:10:00 +0300

    Harjotus14 tehty mutta ei toimi jostaki syystä

---
+++ Harjotus14/Harjotus14/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Harjotus14
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TekstiTB = new System.Windows.Forms.TextBox();
+            this.TallennaBT = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TekstiTB
+            // 
+            this.TekstiTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TekstiTB.Location = new System.Drawing.Point(27, 22);
+            this.TekstiTB.Multiline = true;
+            this.TekstiTB.Name = "TekstiTB";
+            this.TekstiTB.Size = new System.Drawing.Size(742, 341);
+            this.TekstiTB.TabIndex = 0;
+            // 
+            // TallennaBT
+            // 
+            this.TallennaBT.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TallennaBT.Location = new System.Drawing.Point(27, 385);
+            this.TallennaBT.Name = "TallennaBT";
+            this.TallennaBT.Size = new System.Drawing.Size(741, 55);
+            this.TallennaBT.TabIndex = 1;
+            this.TallennaBT.Text = "Tallenna päiväkirjaan";
+            this.TallennaBT.UseVisualStyleBackColor = true;
+            this.TallennaBT.Click += new System.EventHandler(this.TallennaBT_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TallennaBT);
+            this.Controls.Add(this.TekstiTB);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TekstiTB;
+        private System.Windows.Forms.Button TallennaBT;
+    }
+}
+
---
+++ Harjotus14/Harjotus14/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Harjotus14
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void TallennaBT_Click(object sender, EventArgs e)
+        {
+            string teksti = File.ReadAllText(@"C:\\Users\\tomas.roulamo\\source\\repos\\T1-tason_ohjelmointi\\Paivakirja.txt");
+            teksti += TekstiTB.Text;
+            teksti += " " + DateTime.Now.ToString("dd.MM.yyyy HH:mm") + "\n";
+            TextWriter txt = new StreamWriter("C:\\Users\\tomas.roulamo\\source\\repos\\T1-tason_ohjelmointi\\Paivakirja.txt");
+            txt.Write(teksti);
+            txt.Close();
+            Application.Exit();
+        }
+    }
+}
