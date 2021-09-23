8d2aa3ac0f86c67e2bccfed66d93febd608cad16
unknown <tomas.roulamo@edu.keuda.fi>
2021-09-23 09:20:03 +0300

    Teht 17 tehty ja toimii

---
+++ Harjotus17/Harjotus17/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Harjotus17
+{
+    public partial class MuistioForm : Form
+    {
+        private OpenFileDialog openFileDialog;
+        private SaveFileDialog saveFileDialog;
+        private FontDialog fontDialog;
+        public MuistioForm()
+        {
+            InitializeComponent();
+        }
+
+        private void UusiTiedosto()
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(TekstiRTB.Text))
+                {
+                    MessageBox.Show("Sinun pitää tallentaa ensin!");
+                }
+                else
+                {
+                    TekstiRTB.Text = string.Empty;
+                    Text = "Nimetön";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Virhe: " + ex);
+            }
+        }
+
+        private void TallennaTiedosto()
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(TekstiRTB.Text))
+                {
+                    saveFileDialog = new SaveFileDialog();
+                    saveFileDialog.Filter = "Teksti tiedosto (*.txt) | '.txt";
+                    if(saveFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        File.WriteAllText(saveFileDialog.FileName, TekstiRTB.Text);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Virhe: " + ex);
+            }
+        }
+
+        private void AvaaTiedosto()
+        {
+            try
+            {
+                openFileDialog = new OpenFileDialog();
+                
+                if(openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    TekstiRTB.Text = File.ReadAllText(openFileDialog.FileName);
+                    Text = openFileDialog.FileName;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Virhe avattaessa tiedostoa!");
+            }
+        }
+
+        private void MuistioForm_Load(object sender, EventArgs e)
+        {
+            fontDialog = new FontDialog();
+        }
+
+        private void uusiToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            UusiTiedosto();
+        }
+
+        private void avaaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AvaaTiedosto();
+        }
+
+        private void tallennaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            TallennaTiedosto();
+        }
+
+        private void lopetaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(TekstiRTB.Text))
+                {
+                    TallennaTiedosto();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Virhe: " + ex);
+            }
+        }
+
+        private void kirjasinToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(fontDialog.ShowDialog() == DialogResult.OK)
+                {
+                    TekstiRTB.Font = fontDialog.Font;                
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Virhe: " + ex);
+            }
+        }
+    }
+}
